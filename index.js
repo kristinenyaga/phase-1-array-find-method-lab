2c3635a9d6e08966e9a8684ade68c699f4e82efc
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-22 10:36:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ const record = [
   ]
   
   const superbowlWin = (record) => {
-<<<<<<< HEAD
+
     const isWin = record.find(record => record.result === "W");
     return isWin ? isWin.year : undefined;
-=======
-    const win = record.find(record => record.result === "W");
-    return win ? win.year : undefined;
-      
->>>>>>> refs/remotes/origin/master
+
 }
 
